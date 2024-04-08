60922dca3f1a77ae438c51a7d0a441235dc3ea52
faynirnau <naudet.jeremy@laposte.net>
2024-04-08 13:30:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $ville = [
+            "Paris",
+            "Vienna", 
+            "Bratislava",
+            "Londres",
+            "Berlin",
+            "Madrid",
+            "Rome",
+            "Lisbonne",
+            "Budapest",
+            "Varsovie",
+            "Prague"
+        ];
+
+        $links = [
+            ["paris", "vienna"],
+            ["vienna", "bratislava"],
+            ["bratislava", "londres"],
+            ["londres", "berlin"],
+            ["berlin", "madrid"],
+            ["madrid", "rome"],
+            ["rome", "lisbonne"],
+            ["lisbonne", "budapest"],
+            ["budapest", "varsovie"],
+            ["varsovie", "prague"],
+            ["prague", "paris"]
+        ];
+
+        $matrice = [];
+
+        for ($i = 0; $i < count($ville); $i++) {
+            $matrice[$ville[$i]] = [];
+            for ($j = 0; $j < count($ville); $j++) {
+                if ($i == $j) {
+                    $matrice[$ville[$i]][$ville[$j]] = 0;
+                }else {
+                    $matrice[$ville[$i]][$ville[$j]] = random_int(0, 500);
+                }
+            }
+        }
+
+    ?>
+
+    <table style="border-collapse: collapse;">
+        <?php
+            for ($i = 0; $i < count($matrice)+1; $i++) {
+                if ($i >= 1) {
+                    echo "<tr><th>" . $ville[$i-1] . "</th>";
+                }
+                if ($i > 0) {
+                    $i1 = $i-1;
+                }
+                for ($j = 0; $j < count($matrice[$ville[$i]]); $j++) {
+                    if ($i == 0 && $j == 0) {
+                        echo "<th></th>";
+                        echo "<th>" . $ville[$j] . "</th>";
+                    }
+                    elseif ($i == 0 && $j >= 1) {
+                        echo "<th>" . $ville[$j] . "</th>";
+                    }elseif ($i >= 1) {
+                        echo "<td>" . $matrice[$ville[$i1]][$ville[$j]] . "</td>";
+                    }
+                }
+                echo "</tr>";
+            }
+
+        ?>
+    </table>
+</body>
+</html>
+
